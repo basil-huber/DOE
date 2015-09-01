be6ac36fb95799ed2c80ed3c2952f7ec19b40e8f
Basil Huber <basil.huber@gmail.com>
2015-09-01 12:20:59 +0200

    Adjusted plot_report.m and print_plot_latex.m to print plots directly to ../Latex/images

--- matlab/print_plot_latex.m
+++ matlab/print_plot_latex.m
@@ -1,3 +1,3 @@
 function print_plot_latex(filename)
-    matlab2tikz(filename,'width','\figW', 'height','\figH','showInfo', false,'extraAxisOptions','xlabel shift={-4pt}');
+    matlab2tikz(['../Latex/images/' filename],'width','\figW', 'height','\figH','showInfo', false,'extraAxisOptions',{'xlabel shift={-4pt}','ylabel shift={-5pt}'});
 end
